493d182534a995c2a7f326a22f68e3d637f04e98
tcyeee <tcyeee@outlook.com>
2020-07-19 19:49:48 +0800

    文字组件

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:fun_flutter_demo/test/1.dart';
 import 'package:fun_flutter_demo/test/base.dart';
 
-main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 // 页面信息
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return test1();
+    return MaterialApp(
+        // 这个参数用来去除右上角的'debug'横幅
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(appBar: AppBar(title: Text('导航栏上的东西')), body: test1()),
+        theme: ThemeData(primarySwatch: Colors.cyan));
   }
 }
--- lib/test/1.dart
+++ lib/test/1.dart
@@ -6,10 +6,25 @@ class test1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      'hello',
-      textDirection: TextDirection.rtl,
-      style: TextStyle(color: Colors.red, fontSize: 50.0),
-    ));
+        child: Container(
+            alignment: Alignment.center,
+            height: 300.0,
+            width: 300.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                color: Colors.yellow,
+                border: Border.all(color: Colors.red, width: 2.0)),
+            child: Text('hello',
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 3.0,
+                    shadows: [
+                      Shadow(color: Colors.red, offset: Offset(2.0, 2.0)),
+                      Shadow(color: Colors.grey, offset: Offset(-2.0, -2.0))
+                    ]))));
   }
 }
