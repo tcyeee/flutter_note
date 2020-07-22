ad461eb25679137730101de83ec0bdbf9ca75be7
tcyeee <tcyeee@outlook.com>
2020-07-22 21:30:47 +0800

    垂直列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fun_flutter_demo/test/3.dart';
+import 'package:fun_flutter_demo/test/4-list.dart';
 
 main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         // 这个参数用来去除右上角的'debug'横幅
         debugShowCheckedModeBanner: false,
-        home: Scaffold(appBar: AppBar(title: Text('导航栏上的东西')), body: test3()),
+        home: Scaffold(appBar: AppBar(title: Text('导航栏上的东西')), body: Mylist()),
         theme: ThemeData(primarySwatch: Colors.cyan));
   }
 }
