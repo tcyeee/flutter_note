7bb5c8795efa128c7a9cd064e90a39c44f8da154
tcyeee <tcyeee@outlook.com>
2020-07-25 19:19:35 +0800

    绝对布局

---
+++ lib/test/7-stack.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fun_flutter_demo/test/mock.dart';
+
+class StateDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      color: Colors.red[100],
+      child: Stack(children: <Widget>[
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Icon(Icons.send, size: 40, color: Colors.white)),
+        Align(
+            alignment: Alignment.center,
+            child: Icon(Icons.save, size: 40, color: Colors.white)),
+        Align(
+            alignment: Alignment.topRight,
+            child: Icon(Icons.settings, size: 40, color: Colors.white)),
+        Positioned(
+            left: 20,
+            height: 150,
+            child: Icon(Icons.print, size: 40, color: Colors.lightBlue[200]))
+      ]),
+    );
+  }
+}
