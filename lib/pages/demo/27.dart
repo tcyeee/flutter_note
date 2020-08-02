72f038f84ecfa8593590fbf2fca89ab770dc9a27
tcyeee <tcyeee@outlook.com>
2020-08-02 22:16:25 +0800

    单选多选

--- lib/pages/demo/27.dart
+++ lib/pages/demo/27.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fun_flutter_demo/common/mock/mock.dart';
 
 class FormDamo extends StatefulWidget {
   @override
@@ -7,8 +8,14 @@ class FormDamo extends StatefulWidget {
 }
 
 class _FormDamoState extends State<FormDamo> {
+  List data = mock().listData['data'];
+
   var _username = new TextEditingController();
   var _password = new TextEditingController();
+  bool _flag = false;
+  int _sex = 1;
+  int _hasOpen = 1;
+  bool _swich = false;
 
   @override
   void initState() {
@@ -21,7 +28,7 @@ class _FormDamoState extends State<FormDamo> {
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.all(10),
-        child: Column(children: <Widget>[
+        child: ListView(children: <Widget>[
           TextField(
               controller: _username,
               decoration: InputDecoration(hintText: "请输入用户名称")),
@@ -30,7 +37,96 @@ class _FormDamoState extends State<FormDamo> {
               obscureText: true,
               controller: _password,
               decoration: InputDecoration(hintText: "请输入密码")),
-          SizedBox(height: 10),
+          SizedBox(height: 30),
+          TextField(
+            maxLength: 300,
+            maxLines: 3,
+            decoration: InputDecoration(
+                hintText: "请输入备注信息", border: OutlineInputBorder()),
+          ),
+          CheckboxListTile(
+            secondary: Icon(Icons.help),
+            activeColor: Colors.cyan,
+            value: this._flag,
+            onChanged: (bool value) {
+              setState(() {
+                this._flag = value;
+              });
+            },
+            selected: this._flag,
+            title: Text("加珍珠"),
+          ),
+          Divider(),
+          Row(children: <Widget>[
+            Radio(
+              activeColor: Colors.cyan,
+              onChanged: (value) {
+                setState(() {
+                  this._sex = value;
+                });
+              },
+              groupValue: this._sex,
+              value: 1,
+            ),
+            Text("男"),
+            SizedBox(width: 50),
+            Radio(
+              activeColor: Colors.cyan,
+              onChanged: (value) {
+                setState(() {
+                  this._sex = value;
+                });
+              },
+              groupValue: this._sex,
+              value: 2,
+            ),
+            Text("女"),
+          ]),
+          Divider(),
+          RadioListTile(
+            activeColor: Colors.cyan,
+            title: Text("开启权限控制"),
+            subtitle: Text("开启后,一切都不一样了"),
+            value: 1,
+            onChanged: (v) {
+              setState(() {
+                this._hasOpen = v;
+              });
+            },
+            groupValue: this._hasOpen,
+            selected: this._hasOpen == 1,
+            secondary: Image.network(data[3]['img']),
+          ),
+          RadioListTile(
+            activeColor: Colors.cyan,
+            title: Text("关闭权限控制"),
+            subtitle: Text("关闭以后,全部消失"),
+            value: 2,
+            onChanged: (v) {
+              setState(() {
+                this._hasOpen = v;
+              });
+            },
+            groupValue: this._hasOpen,
+            selected: this._hasOpen == 2,
+            secondary: Image.network(data[1]['img']),
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("是否爆炸"),
+              Switch(
+                onChanged: (bool value) {
+                  setState(() {
+                    this._swich = value;
+                  });
+                },
+                value: this._swich,
+              ),
+            ],
+          ),
+          Divider(),
           Container(
               width: double.infinity,
               child: RaisedButton(
@@ -39,7 +135,7 @@ class _FormDamoState extends State<FormDamo> {
                     print(_username.text);
                     print(_password.text);
                   },
-                  child: Text("登录"))),
+                  child: Text("提交"))),
         ]));
   }
 }
